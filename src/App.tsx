088d3d6b6d3e90c160b959456aad53df67b7a8c7
stetstudios-commits <stetstudios@gmail.com>
2026-03-23 11:56:34 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ function App() {
   return (
     <Router>
       <ScrollToTop />
-      <div className="min-h-screen bg-white" style={{ paddingTop: '136px' }}>
+      <div className="min-h-screen bg-white" style={{ paddingTop: '134px' }}>
         <Navigation />
         <Routes>
           <Route path="/" element={<Home />} />
